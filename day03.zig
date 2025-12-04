849b0c23a38ac166abcbd3ed69f97dd9c6eb1cd7
Connor McCarthy <connor15mcc@gmail.com>
2025-12-03 21:48:17 -0500

    day 3 part 2
    I don't like the while-loop comment formatting, but the rest was pretty
    enjoyable!

--- day03.zig
+++ day03.zig
@@ -2,34 +2,64 @@ const std = @import("std");
 
 const batteries = @embedFile("day03.txt");
 
-fn max_joltage(input: []const u8) !usize {
-    var joltage: usize = 0;
+fn total_joltage(alloc: std.mem.Allocator, input: []const u8) !u64 {
+    var joltage: u64 = 0;
 
-    const trimmed = std.mem.trim(u8, input, "\n");
-    var banksIter = std.mem.tokenizeScalar(u8, trimmed, '\n');
-    while (banksIter.next()) |bank| {
-        const max_idx = std.mem.indexOfMax(u8, bank[0 .. bank.len - 1]);
-        const max_battery_s = bank[max_idx];
-        const next_battery_s = std.mem.max(u8, bank[max_idx + 1 ..]);
-
-        joltage += (max_battery_s - '0') * 10 + (next_battery_s - '0');
+    var it = std.mem.tokenizeScalar(u8, input, '\n');
+    while (it.next()) |line| {
+        const bank = std.mem.trim(u8, line, " ");
+        joltage += try max_joltage(alloc, bank);
     }
 
     return joltage;
 }
 
+fn max_joltage(alloc: std.mem.Allocator, bank: []const u8) !u64 {
+    const battery_size = 12;
+    var battery_stack = std.ArrayList(u8).empty;
+    defer battery_stack.deinit(alloc);
+
+    for (0..bank.len, bank) |i, battery| {
+        while (
+        // stack not empty
+        battery_stack.items.len > 0
+            // this batter is larger than the most recent in the stack
+        and battery_stack.getLast() < battery
+            // there are enough elements to fill the rest of the battery array
+        and battery_stack.items.len + (bank.len - i) > battery_size) {
+            _ = battery_stack.pop();
+        }
+        if (battery_stack.items.len < battery_size) {
+            try battery_stack.append(alloc, battery);
+        }
+    }
+
+    return try std.fmt.parseInt(u64, battery_stack.items, 10);
+}
+
 pub fn main() !void {
-    const soln = try max_joltage(batteries);
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer {
+        const deinit_status = gpa.deinit();
+        if (deinit_status == .leak) {
+            @panic("Memory leak detected!");
+        }
+    }
+
+    const alloc = gpa.allocator();
+
+    const soln = try total_joltage(alloc, batteries);
     std.debug.print("Solution: {d}\n", .{soln});
 }
 
 test "provided example" {
+    const alloc = std.testing.allocator;
+
     const input =
         \\ 987654321111111
         \\ 811111111111119
         \\ 234234234234278
         \\ 818181911112111
     ;
-
-    try std.testing.expectEqual(357, max_joltage(input));
+    try std.testing.expectEqual(3121910778619, total_joltage(alloc, input));
 }
