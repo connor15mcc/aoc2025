c1bba5829a9b72b2ea2d5619107206d3bbfc52f9
Connor McCarthy <connor15mcc@gmail.com>
2025-12-07 14:08:26 -0500

    day 7 part 2

--- 2025/day07.zig
+++ 2025/day07.zig
@@ -6,52 +6,24 @@ const beam = '|';
 const splitter = '^';
 
 fn count_splits(alloc: std.mem.Allocator, input: []const u8) !u64 {
-    var it = std.mem.tokenizeScalar(u8, input, '\n');
-    const source_line = it.next().?;
+    var front_it = std.mem.tokenizeScalar(u8, input, '\n');
+    const src_line = front_it.next().?;
 
-    // TODO: maybe better to explicitly use bits instead? unsure how this will
-    // be represented by zig
-    var beams = try alloc.alloc(bool, source_line.len);
-    defer alloc.free(beams);
+    var counts = try alloc.alloc(u64, src_line.len);
+    defer alloc.free(counts);
+    @memset(counts, 1);
 
-    const prev_beams = try alloc.alloc(bool, source_line.len);
-    defer alloc.free(prev_beams);
-
-    const src_idx = std.mem.findScalar(u8, source_line, src).?;
-    beams[src_idx] = true;
-
-    var num_split: u64 = 0;
-    while (it.next()) |line| {
-        defer @memcpy(prev_beams, beams);
-        num_split += split_beams(beams, prev_beams, line);
+    var back_it = std.mem.splitBackwardsScalar(u8, input, '\n');
+    while (back_it.next()) |line| {
+        for (0.., line) |idx, char| {
+            if (char == splitter) {
+                counts[idx] = counts[idx - 1] + counts[idx + 1];
+            }
+        }
     }
 
-    return num_split;
-}
-
-fn split_beams(beams: []bool, prev_beams: []const bool, line: []const u8) u64 {
-    var num_split: u64 = 0;
-    var start: usize = 0;
-    while (std.mem.findScalarPos(u8, line, start, splitter)) |i| {
-        defer start = i + 1;
-
-        // no-op if no beam above
-        if (!prev_beams[i]) continue;
-
-        if (i - 1 >= 0) beams[i - 1] = true;
-        if (i + 1 < line.len) beams[i + 1] = true;
-        beams[i] = false;
-        num_split += 1;
-    }
-    return num_split;
-}
-
-fn printBeams(beams: []const bool) void {
-    for (beams) |b| {
-        const c: u8 = if (b) '|' else '.';
-        std.debug.print("{c}", .{c});
-    }
-    std.debug.print("\n", .{});
+    const src_idx = std.mem.findScalar(u8, src_line, src).?;
+    return counts[src_idx];
 }
 
 pub fn main() !void {
@@ -88,5 +60,5 @@ test "provided example" {
         \\.^.^.^.^.^...^.
         \\...............
     ;
-    try std.testing.expectEqual(21, count_splits(alloc, input));
+    try std.testing.expectEqual(40, count_splits(alloc, input));
 }
